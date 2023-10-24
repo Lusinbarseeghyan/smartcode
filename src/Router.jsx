c5546137ecd64fd27d0cf6f662a43cb5b4f5c386
gevavagian <gavagiann@mail.ru>
2023-10-24 17:03:21 +0400

    fix conflict

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout/Layout";
+
+import Layout from "./Pages/Layout/Layout";
+import Courses from "./pages/Courses/Courses";
+import Features from "./pages/Features/Features";
+import Staff from "./pages/Staff/Staff";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Partners from "./pages/Partners/Partners";
+import NotFound from "./pages/NotFound/NotFound";
 
 const Router = () => {
     return (
