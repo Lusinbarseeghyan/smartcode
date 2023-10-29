231350be94b837d841f42a6d9e77efe0641c73ef
gevavagian <gavagiann@mail.ru>
2023-10-29 16:51:27 +0400

    bugs fixed

--- src/Components/HeaderGridImages/HeaderGridImages.jsx
+++ src/Components/HeaderGridImages/HeaderGridImages.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
 
 import first from "../../assets/images/1.jpg";
 import sec from "../../assets/images/2.jpg";
@@ -6,11 +7,50 @@ import third from "../../assets/images/3.jpg";
 import fivth from "../../assets/images/4.jpg";
 import sixth from "../../assets/images/5.jpg";
 
+import bgVideo from "../../assets/videos/banner.mp4";
+
 import classes from "./HeaderGridImages.module.css";
+import useAnimations from "../../utils/Animations/useAnimations";
 const HeaderGridImages = () => {
+    const { rightAnimationVariant } = useAnimations();
+
+    const videoRef = useRef();
+
+    const handleScroll = () => {
+        const scrollPosition = window.scrollY;
+        const startScalePosition = 30;
+        const endScalePosition = 300;
+        if (scrollPosition > startScalePosition) {
+            let scale =
+                1.2 +
+                (scrollPosition - startScalePosition) /
+                    (endScalePosition - startScalePosition);
+            if (scale > 3) scale = 3;
+            if (videoRef.current) {
+                videoRef.current.style.transform = `translateY(${scrollPosition}px) scale(${scale})`;
+                if (videoRef.current.paused) {
+                    videoRef.current.play();
+                }
+            }
+        } else {
+            if (videoRef.current) {
+                videoRef.current.style.transform =
+                    " translateX(0%) translateY(0) scale(1)";
+                if (!videoRef.current.paused) {
+                    videoRef.current.pause();
+                }
+            }
+        }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
     return (
         <div>
-            <div className={classes.header_images}>
+            <motion.div
+                className={classes.header_images}
+                {...rightAnimationVariant(1)}
+            >
                 <img className={classes.first_image} src={third} alt="" />
 
                 <img
@@ -24,7 +64,9 @@ const HeaderGridImages = () => {
                     src={sec}
                     alt="smartcode"
                 />
-                <div className={classes.forth_image}></div>
+                <div className={classes.forth_image}>
+                    <video ref={videoRef} src={bgVideo} muted></video>
+                </div>
                 <img
                     className={classes.fivth_image}
                     src={fivth}
@@ -35,7 +77,7 @@ const HeaderGridImages = () => {
                     src={sixth}
                     alt="smartcode"
                 />
-            </div>
+            </motion.div>
         </div>
     );
 };
--- src/Components/HomeHeader/HomeHeader.jsx
+++ src/Components/HomeHeader/HomeHeader.jsx
@@ -1,7 +1,6 @@
-import React, { useRef } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 
-import bgVideo from "../../assets/videos/banner.mp4";
 import useAnimations from "../../utils/Animations/useAnimations";
 
 import HeaderGridImages from "../HeaderGridImages/HeaderGridImages";
@@ -9,36 +8,7 @@ import FormBox from "../../components/FormBox/FormBox";
 
 import classes from "./HomeHeader.module.css";
 const HomeHeader = () => {
-    const formRef = useRef();
-    const videoRef = useRef();
-
     const { leftAnimationVariant, opacityAnimationVariant } = useAnimations();
-
-    const handleScroll = () => {
-        const scrollPosition = window.scrollY;
-        const startScalePosition = 30;
-        const endScalePosition = 300;
-        if (scrollPosition > startScalePosition) {
-            let scale =
-                1.2 +
-                (scrollPosition - startScalePosition) /
-                    (endScalePosition - startScalePosition);
-            if (scale > 3) scale = 3;
-            if (videoRef.current) {
-                videoRef.current.style.transform = `translateY(${scrollPosition}px) scale(${scale})`;
-                videoRef.current.play();
-            }
-        } else {
-            if (videoRef.current) {
-                videoRef.current.pause();
-
-                videoRef.current.style.transform =
-                    " translateX(0%) translateY(0) scale(1)";
-            }
-        }
-    };
-
-    window.addEventListener("scroll", handleScroll);
 
     return (
         <div className={classes.header_container}>
@@ -76,19 +46,11 @@ const HomeHeader = () => {
                     >
                         <HeaderGridImages />
                     </motion.div>
-
-                    <motion.div
-                        className={classes.bg_video}
-                        {...leftAnimationVariant(1)}
-                    >
-                        <video ref={videoRef} src={bgVideo} muted></video>
-                    </motion.div>
                 </motion.div>
             </motion.header>
             <div className={` ${classes.form_container}`}>
                 <div className={`container ${classes.form_box}`}>
                     <motion.div
-                        ref={formRef}
                         initial="hidden"
                         whileInView="visible"
                         {...leftAnimationVariant(2)}
